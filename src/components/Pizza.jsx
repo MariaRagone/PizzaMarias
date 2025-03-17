0b2e9f474cb7c868f91534a62b8c755205fe8a94
mragone <maria.ragone@samaritanministries.org>
2025-03-17 09:13:08 -0400

    create lazy route

---
+++ src/components/Pizza.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+const Pizza = (props) => {
+  return (
+    <div className="">
+      <h2>{props.name}</h2>
+      <p>{props.description}</p>
+      <img src={props.image} alt={props.name} />
+    </div>
+  );
+};
+
+export default Pizza;
